213c187d4817c84316067fc6a4ed5b9db93f759f
HuannAnd <huannvicente14@outlook.com>
2023-09-23 16:02:10 -0300

    criando o esqueleto do Context de Games

---
+++ frontend/src/contexts/GamesContextProvider/index.tsx
@@ -0,0 +1,7 @@
+// TODO: Criar o contexto de games para transitar de pag
+
+// TODO: usar o usePathname para pegar a rota e lidar com o s ifs dentro de um useEffect!
+
+// TODO: exportar para Providers.tsx (lá tem todos os contextos)
+
+// E para usar o dado que tu quer transmitir usar um hook global (já criei par você)
